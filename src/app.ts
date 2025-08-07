801b956857d3bc288a753563459a0ea338a4da60
Agop Kalikian <hagopk82@outlook.com>
2025-08-07 09:13:47 +0200

    Setup: zod and Swagger

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,22 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerJsdoc from 'swagger-jsdoc';
+
+const swaggerSpec = swaggerJsdoc({
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Bewerbungsmanager API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/routes/*.ts'],
+});
 
 const app = express();
 
 app.use(express.json());
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.get('/', (_req, res) => {
   res.send('Willkommen beim Bewerbungsmanager!');
